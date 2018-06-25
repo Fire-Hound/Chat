fd8ca83dd07c9c42f94a9611f19c39882cbe69e4
Vikram Cothur <vcothur7@gmail.com>
2018-06-25 18:36:31 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,79 +1,140 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View, TouchableOpacity  } from 'react-native';
-
+import { FlatList, Text, Image, TouchableOpacity, Dimensions, StyleSheet, TextInput, View, KeyboardAvoidingView } from 'react-native';
+import Expo from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import ChatBubble from './Components/ChatBubble'
+import {POST,GET} from './Components/Network'
+let ip = "http://VikramCothur.pythonanywhere.com:/"
 export default class App extends React.Component {
-
-  state ={ isLoading: true}
-
+  state = {
+    currentText: "",
+    Texts: []
+  }
+  onChangeText = (text) => {
+    this.setState({currentText:text})
+  }
+  sendText = async() => {
+    let dataToSend = {user:"vikram", text:this.state.currentText}
+    responseJson = await POST(ip, dataToSend)
+    this.setState({
+      currentText: "",
+      Texts: [{user:"vikram", text:this.state.currentText, },
+       ...this.state.Texts]
+    })
+  }
+  update = async() =>{
+    try{
+    responseJson = await GET(ip, 
+    this.state.Texts.length.toString())
+    }catch(err){console.log(err)}
+    console.log(this.state.Texts.length)
+    if(responseJson==0) return;
+    this.setState({Texts: [...responseJson.reverse(), ...this.state.Texts]})
+  }
+  renderChatBubble = ({item}) => <ChatBubble text={item.text} 
+  isUser={item.user=="vikram"} userName={item.user}/>
 
   componentDidMount(){
-    return this.getAllTodo()
-  }
-  add = () => {
-  this.postTodo()
-  }
-
-  postTodo = () => {
-    fetch('http://192.168.0.7:5000/', {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({todo:"another todo"})
-    })
-    .then(res=>res.json())
-    .then(this.getAllTodo());
-  }
-
-  getAllTodo = () => {
-    fetch('http://192.168.0.7:5000/all/')
-    .then((response) => response.json())
-    .then((responseJson) => {
-
-      this.setState({
-        isLoading: false,
-        dataSource: responseJson,
-      });
-
-    })
-    .catch((error) =>{
-      console.error(error);
-    });
-  }
-  render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
+    this.interval = setInterval(this.update, 4000)
     }
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+  render() {
+    return (
+        <KeyboardAvoidingView enabled behavior="padding" 
+        style={styles.container}>
+          <View style={styles.header}>
 
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
-      <FlatList 
-      data={this.state.dataSource} 
-      renderItem={({item})=>{
-        return(<Text> {item.todo} </Text>);
-      }}
-      keyExtractor={(item, index)=>index.toString()}
-      />
-      <View style={{flexDirection: 'row', justifyContent: "space-between"}}>
-      <TouchableOpacity 
-      onPress={this.add}
-      style={{backgroundColor:'#000044', height:50, flex: 1,
-      justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color:'white', fontSize: 20}}>Add Todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-      style={{backgroundColor:'#000044', height:50, flex: 1,
-      justifyContent: 'center', alignItems: 'center', borderLeftWidth:0.5, borderColor: 'white' }}>
-        <Text style={{color:'white', fontSize: 20}}>Refresh</Text>
-      </TouchableOpacity>
-      </View>
-      </View>
+              <Image 
+              source={require('./Assets/StillVariables.jpg')}
+              style={styles.groupImage}
+              />
+      
+            <View style={styles.groupName}>
+              <Text style={{fontSize: 24}}>
+                Still Variables
+              </Text>
+            </View>
+          </View>
+
+          
+          <FlatList contentContainerStyle={styles.body}
+          style={{flex: 1, backgroundColor: "white",}}
+          data={this.state.Texts}
+          renderItem={this.renderChatBubble}
+          keyExtractor={(item, index)=>index.toString()}
+          inverted
+          />
+          
+          <View style={styles.footer}>
+            <View style={styles.keyboard}>
+
+              <TextInput placeholder="Start Typing" 
+              style={styles.input} 
+              underlineColorAndroid="transparent"
+              value={this.state.currentText}
+              onChangeText={this.onChangeText}
+              />
+
+            </View>
+            <TouchableOpacity style={styles.send} onPress={this.sendText}>
+              <Ionicons name="md-send" color="blue" size={30}/>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAvoidingView>
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eeeeee",
+    marginTop: Expo.Constants.statusBarHeight
+  },
+  header: {
+    opacity: 0.7,
+    height: 55,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+  groupImage:{
+    height: 40,
+    width: 40,
+    borderRadius: 20
+  },
+  groupName:{
+    height: 40,
+    width: 200,
+    justifyContent: "center",
+    marginLeft: 10,
+  },
+  body:{
+    alignItems: "flex-end"
+  },
+  footer: {
+    backgroundColor: "#eeeeee",
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  keyboard: {
+    backgroundColor: "white",
+    width: Dimensions.get("window").width - 90,
+    flexDirection: "row",
+    justifyContent: "center",
+    borderRadius: 20
+  },
+  input:{
+    flex:1,
+    fontSize:20, 
+    marginLeft: 20
+  },
+  send:{
+    marginLeft: 12
+  }
+});
